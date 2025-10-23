632d8ba3f5f1cbe00c71159f41af45b6af9e8b70
nabhon <nabhon.k@gmail.com>
2025-10-24 01:03:24 +0700

    add(projects): เพิ่มตาราง project และปุ่มเพื่ม project และ api ลบ project

--- src/app/moderation/projects/page.tsx
+++ src/app/moderation/projects/page.tsx
@@ -1,12 +1,15 @@
-import CreateProjectForm from "@/components/form/create-project-form"
+import AddProjectButton from "./project-add-button"
+import ProjectsTable from "./project-table"
 
 export default function Page() {
   return (
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0 items-center">
-          <div className="flex flex-col w-full max-w-[768px]">
-            <h1 className="text-2xl font-bold">Create New Project</h1>
-            
-            <CreateProjectForm/>
+        <div className="flex flex-1 flex-col gap-4 pt-0 items-center">
+          <div className="flex flex-col w-full px-4">
+            <div className="flex flex-row w-full mb-2">
+              <h1 className="text-2xl flex-1 font-bold">Manage Project</h1>
+              <AddProjectButton />
+            </div>    
+            <ProjectsTable/>
           </div>
         </div>
   )
